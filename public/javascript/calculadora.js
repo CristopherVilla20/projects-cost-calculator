8c2a66d9b1942af4e2df1ea623022bc3a0175bab
CristopherVilla20 <cgvilla@espol.edu.ec>
2023-06-16 20:30:25 -0500

    agregando mas elementos y resultado final

--- public/javascript/calculadora.js
+++ public/javascript/calculadora.js
@@ -8,14 +8,31 @@ function funcionPredeterminada() {
         let valorPorHora = valorPorHoraHTML.value;
         
         let interfazHorasHTML = document.getElementById("interfazHoras");
-        let valorInterfazPorHora = interfazHorasHTML.value;
-        
-        let interfazTestHTML = document.getElementById("interfazTest");
-        let valorInterfazTest = interfazTestHTML.value;
-        
-        let valorTotalHTML = document.getElementById("valorTotal");
-        valorTotalHTML.value = (valorPorHora * valorInterfazPorHora) + (valorPorHora * valorInterfazTest) ;
+        let valorInterfazHoras = parseInt(interfazHorasHTML.value);
 
+        let estructuraHorasHTML = document.getElementById("estructuraHoras");
+        let valorEstructuraHoras = parseInt(estructuraHorasHTML.value);
+ 
+        let estilizacionHorasHTML = document.getElementById("estilizacionHoras");
+        let valorEstilizacionHoras = parseInt(estilizacionHorasHTML.value);
+    
+        let logicaJSHorasHTML = document.getElementById("logicaJSHoras");
+        let valorLogicaJSHoras = parseInt(logicaJSHorasHTML.value);
+
+        let integracionAPIHorasHTML = document.getElementById("integracionAPIHoras");
+        let valorIntegracionAPIHoras = parseInt(integracionAPIHorasHTML.value);
+
+        let otrasFuncionesHorasHTML = document.getElementById("otrasFuncionesHoras");
+        let valorOtrasFuncionesHoras = parseInt(otrasFuncionesHorasHTML.value);
+
+        let testHorasHTML = document.getElementById("testHoras");
+        let valorTestHoras = parseInt(testHorasHTML.value);
+
+      
+        let valorTotalHTML = document.getElementById("valorTotal");
+        valorTotalHTML.value = "$" + valorPorHora * (valorInterfazHoras + valorEstructuraHoras + valorEstilizacionHoras
+            + valorLogicaJSHoras + valorIntegracionAPIHoras + valorOtrasFuncionesHoras + valorTestHoras
+            );
 
     });
     
