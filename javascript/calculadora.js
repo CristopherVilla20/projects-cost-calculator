5eb9c92743f366f8a41f790c37d8e42828827cf6
CristopherVilla20 <cgvilla@espol.edu.ec>
2023-06-18 13:54:19 -0500

    validando ingreso de datos

--- javascript/calculadora.js
+++ javascript/calculadora.js
@@ -1,39 +1,38 @@
 
 function funcionPredeterminada() {
     const formulario = document.getElementById('formulario');
-    formulario.addEventListener('submit',(evento)=>{
-        evento.preventDefault();
-        let valorPorHoraHTML = document.getElementById("valorPorHora");
-        let valorPorHora = valorPorHoraHTML.value;
-        
-        let interfazHorasHTML = document.getElementById("interfazHoras");
-        let valorInterfazHoras = parseInt(interfazHorasHTML.value);
-
-        let estructuraHorasHTML = document.getElementById("estructuraHoras");
-        let valorEstructuraHoras = parseInt(estructuraHorasHTML.value);
- 
-        let estilizacionHorasHTML = document.getElementById("estilizacionHoras");
-        let valorEstilizacionHoras = parseInt(estilizacionHorasHTML.value);
-    
-        let logicaJSHorasHTML = document.getElementById("logicaJSHoras");
-        let valorLogicaJSHoras = parseInt(logicaJSHorasHTML.value);
-
-        let integracionAPIHorasHTML = document.getElementById("integracionAPIHoras");
-        let valorIntegracionAPIHoras = parseInt(integracionAPIHorasHTML.value);
-
-        let otrasFuncionesHorasHTML = document.getElementById("otrasFuncionesHoras");
-        let valorOtrasFuncionesHoras = parseInt(otrasFuncionesHorasHTML.value);
-
-        let testHorasHTML = document.getElementById("testHoras");
-        let valorTestHoras = parseInt(testHorasHTML.value);
-
-      
-        let valorTotalHTML = document.getElementById("valorTotal");
-        valorTotalHTML.value = "$" + valorPorHora * (valorInterfazHoras + valorEstructuraHoras + valorEstilizacionHoras
-            + valorLogicaJSHoras + valorIntegracionAPIHoras + valorOtrasFuncionesHoras + valorTestHoras
-            );
-
+    formulario.addEventListener('submit', (evento) => {
+      evento.preventDefault();
+  
+      let valorPorHoraHTML = document.getElementById("valorPorHora");
+      let valorPorHora = parseFloat(valorPorHoraHTML.value) || 0;
+  
+      let interfazHorasHTML = document.getElementById("interfazHoras");
+      let valorInterfazHoras = parseInt(interfazHorasHTML.value) || 0;
+  
+      let estructuraHorasHTML = document.getElementById("estructuraHoras");
+      let valorEstructuraHoras = parseInt(estructuraHorasHTML.value) || 0;
+  
+      let estilizacionHorasHTML = document.getElementById("estilizacionHoras");
+      let valorEstilizacionHoras = parseInt(estilizacionHorasHTML.value) || 0;
+  
+      let logicaJSHorasHTML = document.getElementById("logicaJSHoras");
+      let valorLogicaJSHoras = parseInt(logicaJSHorasHTML.value) || 0;
+  
+      let integracionAPIHorasHTML = document.getElementById("integracionAPIHoras");
+      let valorIntegracionAPIHoras = parseInt(integracionAPIHorasHTML.value) || 0;
+  
+      let otrasFuncionesHorasHTML = document.getElementById("otrasFuncionesHoras");
+      let valorOtrasFuncionesHoras = parseInt(otrasFuncionesHorasHTML.value) || 0;
+  
+      let testHorasHTML = document.getElementById("testHoras");
+      let valorTestHoras = parseInt(testHorasHTML.value) || 0;
+  
+      let valorTotalHTML = document.getElementById("valorTotal");
+      valorTotalHTML.value = "$" + valorPorHora * (valorInterfazHoras + valorEstructuraHoras + valorEstilizacionHoras
+        + valorLogicaJSHoras + valorIntegracionAPIHoras + valorOtrasFuncionesHoras + valorTestHoras);
     });
-    
-}
-funcionPredeterminada();
+  }
+  
+  funcionPredeterminada();
+  
